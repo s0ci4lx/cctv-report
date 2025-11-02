9dd78044b3cf047ef68712d0eda3a2c541d37653
Witchapol Leesen <s0ci4lx@Mac-mini-Witchapol.local>
2025-11-02 23:40:47 +0700

    แก้ไขคำศัพท์ มอบหมาย ยังไม่มอบหมาย

--- src/views/inspector/InspectorCameras.vue
+++ src/views/inspector/InspectorCameras.vue
@@ -367,13 +367,13 @@ onUnmounted(() => {
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                     </svg>
-                    มอบหมาย
+                    Assigned
                   </div>
                   <div v-else class="badge badge-warning gap-1">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
-                    ยังไม่ได้
+                    Available
                   </div>
                 </td>
                 <td>
@@ -450,13 +450,13 @@ onUnmounted(() => {
               <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
               </svg>
-              มอบหมาย
+              มอบหมายแล้ว
             </div>
             <div v-else class="badge badge-warning badge-lg gap-2">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
-              ยังไม่ได้
+              ยังไม่มอบหมาย
             </div>
           </div>
 
